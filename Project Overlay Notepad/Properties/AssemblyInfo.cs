3620743ebf85f43b43a4b18c99d04116f8a98f2e
Nicolas Ho <Nicci.h100@LIVE.DE>
2022-06-20 21:12:05 +0200

    Versioning fixed (YYYY-MM-DD-AutoIncrement(always))

--- Project Overlay Notepad/Properties/AssemblyInfo.cs
+++ Project Overlay Notepad/Properties/AssemblyInfo.cs
@@ -51,5 +51,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2021.5.31.129")]
-[assembly: AssemblyFileVersion("2021.5.31.129")]
+[assembly: AssemblyVersion("2022.6.20.130")]
+[assembly: AssemblyFileVersion("2022.6.20.130")]
